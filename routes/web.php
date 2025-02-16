b31ff0f21c29ab742abc9c90abe1ede2ef6e6db9
Tariqul <tariqul18033@gmail.com>
2025-02-16 16:22:36 +0600

    frontend routes added

--- routes/web.php
+++ routes/web.php
@@ -4,38 +4,30 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\TokenVerificationMiddware;
 
-// test
+// frontend routes
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/user-register', function () {
-    return view('test');
-    
-});
-Route::get('/user-login', function () {
-    return view('test2');
-    
-});
+Route::get('/',[HomeController::class,'HomePage']);
+Route::get('/userLogin',[UserController::class,'LoginPage']);
+Route::get('/userRegistration',[UserController::class,'RegistrationPage']);
+Route::get('/sendOtp',[UserController::class,'SendOtpPage']);
+Route::get('/verifyOtp',[UserController::class,'VerifyOTPPage']);
+Route::get('/resetPassword',[UserController::class,'ResetPasswordPage'])->middleware([TokenVerificationMiddleware::class]);
+Route::get('/dashboard',[DashboardController::class,'DashboardPage'])->middleware([TokenVerificationMiddleware::class]);
+Route::get('/userProfile',[UserController::class,'ProfilePage'])->middleware([TokenVerificationMiddleware::class]);
+Route::get('/categoryPage',[CategoryController::class,'CategoryPage'])->middleware([TokenVerificationMiddleware::class]);
+Route::get('/customerPage',[CustomerController::class,'CustomerPage'])->middleware([TokenVerificationMiddleware::class]);
+Route::get('/productPage',[ProductController::class,'ProductPage'])->middleware([TokenVerificationMiddleware::class]);
+Route::get('/invoicePage',[InvoiceController::class,'InvoicePage'])->middleware([TokenVerificationMiddleware::class]);
+Route::get('/salePage',[InvoiceController::class,'SalePage'])->middleware([TokenVerificationMiddleware::class]);
+Route::get('/reportPage',[ReportController::class,'ReportPage'])->middleware([TokenVerificationMiddleware::class]);
+
 
 
 
 
 Route::post('/user-register', [UserController::class, 'userRegister']);
-
 Route::post('/user-login', [UserController::class, 'userLogin']);
-
-
-//sendOTPCode
-
 Route::post('/send-otp', [UserController::class, 'sendOTPCode']);
-
-//verifyOTP
-
 Route::post('/verify-otp', [UserController::class, 'verifyOTP']);
-
-
-//resetPassword
-
 Route::post('/reset-password', [UserController::class, 'resetPassword'])
 ->middleware(TokenVerificationMiddware::class);
