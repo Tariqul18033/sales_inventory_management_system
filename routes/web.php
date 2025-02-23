74038fd4a97412da52497342a64fa41be073e376
Tariqul <tariqul18033@gmail.com>
2025-02-23 13:47:05 +0600

    catagory added

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\TokenVerificationMiddleware;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\CategoryController;
 
 // frontend routes
 
@@ -34,7 +35,23 @@ Route::post('/send-otp', [UserController::class, 'SendOTPCode']);
 Route::post('/verify-otp', [UserController::class, 'VerifyOTP']);
 Route::post('/reset-password', [UserController::class, 'ResetPassword'])
 ->middleware(TokenVerificationMiddleware::class);
+Route::post('/dashboard', [DashboardController::class, 'DashboardPage'])
 ->middleware([TokenVerificationMiddleware::class]);
 
 // user logout
 Route::get('/logout', [UserController::class, 'UserLogout']);
+
+
+
+
+// Category API
+Route::post("/create-category",[CategoryController::class,'CategoryCreate'])
+->middleware([TokenVerificationMiddleware::class]);
+Route::get("/list-category",[CategoryController::class,'CategoryList'])
+->middleware([TokenVerificationMiddleware::class]);
+Route::post("/delete-category",[CategoryController::class,'CategoryDelete'])
+->middleware([TokenVerificationMiddleware::class]);
+Route::post("/update-category",[CategoryController::class,'CategoryUpdate'])
+->middleware([TokenVerificationMiddleware::class]);
+Route::post("/category-by-id",[CategoryController::class,'CategoryByID'])
+->middleware([TokenVerificationMiddleware::class]);
